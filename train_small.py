e4aec1de26ac599b885b2c68e169682bc9c9e0ce
AmanVernekar <amansuraj.av@gmail.com>
2023-07-01 13:46:47 +0100

    test train_small

--- train_small.py
+++ train_small.py
@@ -2,4 +2,5 @@
 from graph_weather import AnalysisDataset
 
 ds = AnalysisDataset(['/local/scratch-2/asv34/graph_weather/out.zarr'], '/local/scratch-2/asv34/graph_weather/ls_mask.zarr', 0, 0, 1)
-print("Hi")
+print(ds[0])
+print(ds[0].shape)
