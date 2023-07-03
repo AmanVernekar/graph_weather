2401b631c468de1758da0af12cd735603e0a1fc8
AmanVernekar <amansuraj.av@gmail.com>
2023-07-03 13:09:51 +0100

    changed file names and added in gitignore for new dataset

--- train_small.py
+++ train_small.py
@@ -1,6 +1,6 @@
 #from __future__ import absolute_import
 from graph_weather import AnalysisDataset
 
-ds = AnalysisDataset(['/local/scratch-2/asv34/graph_weather/out.zarr'], '/local/scratch-2/asv34/graph_weather/ls_mask.zarr', 0, 0, 1)
+ds = AnalysisDataset(['/local/scratch-2/asv34/graph_weather/20220101.zarr.zip'], '/local/scratch-2/asv34/graph_weather/ls_mask.zarr', 0, 0, 1)
 print(ds[0])
 print(ds[0].shape)
