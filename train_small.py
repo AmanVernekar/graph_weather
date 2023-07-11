c710dc256c517f67ed60e99cf2616ed28fa4219c
AmanVernekar <amansuraj.av@gmail.com>
2023-07-11 10:34:21 +0100

    test using single hour datasets

--- train_small.py
+++ train_small.py
@@ -1,6 +1,16 @@
 #from __future__ import absolute_import
 from graph_weather import AnalysisDataset, GraphWeatherForecaster
+import glob
 
-ds = AnalysisDataset(['/local/scratch-2/asv34/graph_weather/20220101.zarr.zip', '/local/scratch-2/asv34/graph_weather/20220102.zarr.zip'], '/local/scratch-2/asv34/graph_weather/ls_mask.zarr', 0, 0, 1)
+filepaths = glob.glob("/local/scratch-2/asv34/graph_weather/dataset/one_day/*")
+
+ds = AnalysisDataset(filepaths, '/local/scratch-2/asv34/graph_weather/ls_mask.zarr', 0, 0, 1)
+
 print(ds[0])
 print(ds[0].shape)
+
+print(ds[5])
+print(ds[5].shape)
+
+print(ds[22])
+print(ds[22].shape)
