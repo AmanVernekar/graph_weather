ac3f8f7ab7aee71182e254e084bc95d0955332fb
AmanVernekar <amansuraj.av@gmail.com>
2023-07-12 11:35:00 +0100

    checking model size

--- train_small.py
+++ train_small.py
@@ -23,6 +23,16 @@ dataset = DataLoader(ds, batch_size=1, num_workers=32)
 model = GraphWeatherForecaster(lat_lons, num_blocks=2).to(device)
 optimizer = optim.AdamW(model.parameters(), lr=0.001)
 
+param_size = 0
+for param in model.parameters():
+    param_size += param.nelement() * param.element_size()
+buffer_size = 0
+for buffer in model.buffers():
+    buffer_size += buffer.nelement() * buffer.element_size()
+
+size_all_mb = (param_size + buffer_size) / 1024**2
+print('model size: {:.3f}MB'.format(size_all_mb))
+
 print("Done Setup")
 import time
 
