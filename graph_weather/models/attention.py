45824b06c360c3fb5db6d90259def672f9424d74
AmanVernekar <amansuraj.av@gmail.com>
2023-07-25 00:18:51 +0100

    created modulelist and paramlist

--- graph_weather/models/attention.py
+++ graph_weather/models/attention.py
@@ -33,30 +33,10 @@ class ParallelForecaster(torch.nn.Module):
             output_dim = self.feature_dim
 
         self.num_steps = num_steps
-        # self.models = []
-        # for i in range(num_steps):
-        #     self.models.append(
-        #         GraphWeatherForecaster(
-        #             lat_lons=lat_lons,
-        #             resolution=resolution,
-        #             feature_dim=feature_dim,
-        #             aux_dim=aux_dim,
-        #             output_dim=output_dim,
-        #             node_dim=node_dim,
-        #             edge_dim=edge_dim,
-        #             num_blocks=num_blocks,
-        #             hidden_dim_processor_node=hidden_dim_processor_node,
-        #             hidden_dim_processor_edge=hidden_dim_processor_edge,
-        #             hidden_layers_processor_node=hidden_layers_processor_node,
-        #             hidden_layers_processor_edge=hidden_layers_processor_edge,
-        #             hidden_dim_decoder=hidden_dim_decoder,
-        #             hidden_layers_decoder=hidden_layers_decoder,
-        #             norm_type=norm_type,
-        #             use_checkpointing=use_checkpointing
-        #         ).to(torch.device('cuda'))
-        #     )
-        
-        self.model1 = GraphWeatherForecaster(
+        self.models = nn.ModuleList
+        for i in range(num_steps):
+            self.models.append(
+                GraphWeatherForecaster(
                     lat_lons=lat_lons,
                     resolution=resolution,
                     feature_dim=feature_dim,
@@ -74,95 +54,37 @@ class ParallelForecaster(torch.nn.Module):
                     norm_type=norm_type,
                     use_checkpointing=use_checkpointing
                 )
-        
-        self.model2 = GraphWeatherForecaster(
-                    lat_lons=lat_lons,
-                    resolution=resolution,
-                    feature_dim=feature_dim,
-                    aux_dim=aux_dim,
-                    output_dim=output_dim,
-                    node_dim=node_dim,
-                    edge_dim=edge_dim,
-                    num_blocks=num_blocks,
-                    hidden_dim_processor_node=hidden_dim_processor_node,
-                    hidden_dim_processor_edge=hidden_dim_processor_edge,
-                    hidden_layers_processor_node=hidden_layers_processor_node,
-                    hidden_layers_processor_edge=hidden_layers_processor_edge,
-                    hidden_dim_decoder=hidden_dim_decoder,
-                    hidden_layers_decoder=hidden_layers_decoder,
-                    norm_type=norm_type,
-                    use_checkpointing=use_checkpointing
-                )        
-        
-        self.model3 = GraphWeatherForecaster(
-                    lat_lons=lat_lons,
-                    resolution=resolution,
-                    feature_dim=feature_dim,
-                    aux_dim=aux_dim,
-                    output_dim=output_dim,
-                    node_dim=node_dim,
-                    edge_dim=edge_dim,
-                    num_blocks=num_blocks,
-                    hidden_dim_processor_node=hidden_dim_processor_node,
-                    hidden_dim_processor_edge=hidden_dim_processor_edge,
-                    hidden_layers_processor_node=hidden_layers_processor_node,
-                    hidden_layers_processor_edge=hidden_layers_processor_edge,
-                    hidden_dim_decoder=hidden_dim_decoder,
-                    hidden_layers_decoder=hidden_layers_decoder,
-                    norm_type=norm_type,
-                    use_checkpointing=use_checkpointing
-                )
+            )
 
         self.model_type = model_type
 
         if model_type == 'linear':
             self.final_layer = nn.Linear(num_steps*output_dim, output_dim)
         elif model_type == 'params':
-            # self.params = [nn.Parameter(data=torch.tensor([1/self.num_steps])) for i in range(self.num_steps)]
-            self.param1 = nn.Parameter(data=torch.tensor([1/self.num_steps]))
-            self.param2 = nn.Parameter(data=torch.tensor([1/self.num_steps]))
-            self.param3 = nn.Parameter(data=torch.tensor([1/self.num_steps]))
+            self.params = nn.ParameterList([nn.Parameter(data=torch.tensor([1/self.num_steps])) for i in range(self.num_steps)])
         elif model_type == 'simple_attention':
             self.attention_layer = nn.Linear(num_steps*output_dim, num_steps, bias=False)
             self.leaky = nn.LeakyReLU(0.2) 
             self.soft = nn.Softmax(dim=1)   
 
     def forward(self, features):
-        # out = []
-        # for i in range(self.num_steps):
-        #     inp = torch.stack([features[0][i]])
-        #     out.append(self.models[i](inp.to(features.device)))
-        # return self.final_layer(torch.cat(out, dim=-1))
-
-        # out = torch.zeros([features[0][0]].shape[0], [features[0][0]].shape[1])
-        # for i in range(self.num_steps):
-        #     inp = torch.stack([features[0][i]])
-        #     out += self.params[i]*self.models[i](inp.to(features.device))
-        # return out
-
+        features = features[0]
+        
         if self.model_type == 'linear':
-            out = []
-            out.append(self.model1(torch.stack([features[0][0]]).to(features.device)))
-            out.append(self.model2(torch.stack([features[0][1]]).to(features.device)))
-            out.append(self.model3(torch.stack([features[0][2]]).to(features.device)))
+            out = [self.models[i](torch.stack([features[i]]).to(features.device)) for i in range(self.num_steps)]
             return self.final_layer(torch.cat(out, dim=-1))
 
         elif self.model_type == 'params':
-            out = torch.zeros(features[0][0].shape[0], features[0][0].shape[1]).to(features.device)
-            out += self.param1*self.model1(torch.stack([features[0][0]]).to(features.device))[0]
-            out += self.param2*self.model2(torch.stack([features[0][1]]).to(features.device))[0]
-            out += self.param3*self.model3(torch.stack([features[0][2]]).to(features.device))[0]
+            out = torch.zeros(features[0].shape[0], features[0].shape[1]).to(features.device)
+            for i in range(self.num_steps):
+                out += self.params[i]*self.models[i](torch.stack([features[i]]).to(features.device))[0]
             return torch.stack([out])
         
         elif self.model_type == 'simple_attention':
-            embedding = []
-            embedding.append(self.model1(torch.stack([features[0][0]]).to(features.device))[0])
-            embedding.append(self.model2(torch.stack([features[0][1]]).to(features.device))[0])
-            embedding.append(self.model3(torch.stack([features[0][2]]).to(features.device))[0])
-            
+            embedding = [self.models[i](torch.stack([features[i]]).to(features.device))[0] for i in range(self.num_steps)]
             alphas = self.soft(self.leaky(self.attention_layer(torch.cat(embedding, dim=-1))))
 
-            out = torch.zeros(features[0][0].shape[0], features[0][0].shape[1]).to(features.device) 
+            out = torch.zeros(features[0].shape[0], features[0].shape[1]).to(features.device) 
             for i, coeffs in enumerate(alphas):
                 for j in range(self.num_steps):
                     out[i] = out[i] + coeffs[j]*embedding[j][i]
