564792def5c54c24407c160c2f96bcbe12a2ed6c
AmanVernekar <amansuraj.av@gmail.com>
2023-07-25 17:23:51 +0100

    debug

--- test_models.py
+++ test_models.py
@@ -2,7 +2,7 @@ import torch
 
 filepath = '/local/scratch-2/asv34/graph_weather/dataset/models/2022_4months_normed_single_lr4_100epochs.pt'
 model_dict = torch.load(filepath)
-model_dict['']
+# model_dict['']
 
 print("Model's state_dict:")
 for param_tensor in model_dict:
