bf2aaa07b272199bf4e0ac2e7912811bb2031611
AmanVernekar <amansuraj.av@gmail.com>
2023-07-25 17:17:27 +0100

    created test_models

---
+++ test_models.py
@@ -0,0 +1,8 @@
+import torch
+
+filepath = '/local/scratch-2/asv34/graph_weather/dataset/models/2022_4months_normed_parallel3_linlayer_100epochs.pt'
+model_dict = torch.load(filepath)
+
+print("Model's state_dict:")
+for param_tensor in model_dict:
+    print(param_tensor, "\t", model_dict[param_tensor].size())
