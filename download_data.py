27f63cede2ac1b29b499f778cb497fe42853fa8e
AmanVernekar <amansuraj.av@gmail.com>
2023-09-21 17:11:04 +0100

    download test data for uk and europe

--- download_data.py
+++ download_data.py
@@ -2,15 +2,30 @@ import cdsapi
 import xarray as xr
 import zarr
 import numcodecs
+import sys
 
 c = cdsapi.Client()
 uk_area = [63, -10, 47, 4]
 europe_area = [82, -38, 28, 32]
 
-for year in [2022]:
-    for month in [1, 4, 7, 10]:
+train_months = [1, 4, 7, 10]
+test_months = [3, 6, 9, 12]
+years = [2022]
+times = ['00:00', '06:00', '12:00', '18:00']
+
+region = sys.argv[1]
+months = train_months if sys.argv[2] == 'train' else test_months
+
+if region == 'uk':
+    area = uk_area
+elif region == 'europe':
+    area = europe_area
+
+
+for year in years:
+    for month in months:
         for day in range(1,32):
-            for time in ['00:00', '06:00', '12:00', '18:00']:
+            for time in times:
                 try:
                     c.retrieve(
                         'reanalysis-era5-pressure-levels',
@@ -27,15 +42,15 @@ for year in [2022]:
                             'month': str(month).zfill(2),
                             'day': str(day).zfill(2),
                             'time': time,
-                            'area': europe_area,
+                            'area': area,
                             'format': 'netcdf',
                         },
-                        f'/local/scratch-2/asv34/graph_weather/dataset/europe_2022/download_air.nc')
-                    data = xr.open_dataset("/local/scratch-2/asv34/graph_weather/dataset/europe_2022/download_air.nc", engine="netcdf4")
+                        f'/local/scratch-2/asv34/graph_weather/dataset/final/{region}/{region}_{year}/download_air.nc')
+                    data = xr.open_dataset(f'/local/scratch-2/asv34/graph_weather/dataset/final/{region}/{region}_{year}/download_air.nc', engine="netcdf4")
                     #print(data)
                     encoding = {var: {"compressor": numcodecs.get_codec(dict(id="zlib", level=5))} for var in data.data_vars}
                     d = data.chunk({"time": 1})
-                    with zarr.ZipStore(f'/local/scratch-2/asv34/graph_weather/dataset/europe_2022/{year}_{str(month).zfill(2)}_{str(day).zfill(2)}_{time[:2]}.zarr.zip', mode='w') as store:
+                    with zarr.ZipStore(f'/local/scratch-2/asv34/graph_weather/dataset/final/{region}/{region}_{year}/{year}_{str(month).zfill(2)}_{str(day).zfill(2)}_{time[:2]}.zarr.zip', mode='w') as store:
                         d.to_zarr(store, encoding=encoding, compute=True)
                 except:
                     continue
